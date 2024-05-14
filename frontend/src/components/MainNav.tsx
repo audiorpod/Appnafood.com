6cac942ee694b6103661decc0fbdc94a0816f7d4
Rupesh Sahu <rupeshsahu@192.168.1.2>
2024-05-14 13:43:40 +0530

    working with auth

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,10 +1,24 @@
+//import { useAuth0 } from "@auth0/auth0-react";
+import { useAuth0} from "@auth0/auth0-react";
 import { Button } from "./ui/button";
+import { Link } from "react-router-dom";
+
 
 const MainNav = () => {
+ const { loginWithRedirect } = useAuth0();
+
+
   return (
-  <Button variant="ghost" className="font-bold hover:text-orange-500 hover:bg-white">
+ 
+
+  <Button 
+  variant="ghost" 
+  className="font-bold hover:text-orange-500 hover:bg-white"
+  onClick={ async () => await loginWithRedirect()}
+  >
     Log In
   </Button>
+  
   );
 };
 
